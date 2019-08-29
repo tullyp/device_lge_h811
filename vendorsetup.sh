5c2731a5c35b4ad156809fb8920ced3569602fd7
steadfasterX <steadfasterX@gmail.com>
2019-08-29 18:38:26 +0200

    g4: speed up by LLVM_ENABLE_THREADS
    Change-Id: Ia8b09252fb0975f4605ded64e89b553e5e6f9764

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,4 @@
-add_lunch_combo aoscp_h811-userdebug
-add_lunch_combo aoscp_h811-user
+export ANDROID_SET_JAVA_HOME=true
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export USE_NINJA=true
+export LLVM_ENABLE_THREADS=1
