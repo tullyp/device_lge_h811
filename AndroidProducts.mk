e075c9153cd8f09714109080bf74cedeaf00efc4
tullyp <54035666+tullyp@users.noreply.github.com>
2021-03-21 18:22:48 -0400

    Create AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,7 +16,7 @@
 # the License for the specific language governing 
 # permissions and limitations under the License.
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_h811.mk
+    $(LOCAL_DIR)/havoc_vs986_usu.mk
 
 # Screen density
 # must be defined here as they get readonly later on
